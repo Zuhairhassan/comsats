de53e1879eed9a0984ce9f19e64bc02c3332c629
hp <hp@hp-PC>
2016-06-06 08:34:07 +0500

    change

--- src/filehandling/Aclass.java
+++ src/filehandling/Aclass.java
@@ -5,6 +5,13 @@ import java.io.*;
 public class Aclass {
 	public void checkunexcepted()
 	{
+		///
+		///
+		////
+		///
+		////
+		///
+		
 		Scanner enter=new Scanner(System.in);
 		System.out.println("enter the first no");
 		int firstno=enter.nextInt();
